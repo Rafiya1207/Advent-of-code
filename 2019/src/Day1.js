d81f4af6051c90ef1597e0d05be3221435797f34
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2025-12-03 11:02:08 +0530

    add puzzle1part2

--- 2019/src/Day1.js
+++ 2019/src/Day1.js
@@ -1,13 +1,42 @@
-import { sumOf } from "jsr:@std/collections";
+import { runningReduce, sumOf } from "jsr:@std/collections";
 
-export const calculateFuelRequired = (mass) => Math.floor(parseInt(mass) / 3) - 2;
+export const calculateFuelRequired = (mass) =>
+  Math.floor(parseInt(mass) / 3) - 2;
 
-const puzzle1Part1 = () => {
+export const calculateFuelForFuel = (mass) => {
+  const fuels = [];
+  let currentFuel = calculateFuelRequired(mass);
+
+  while (currentFuel >= 0) {
+    fuels.push(currentFuel);
+    currentFuel = calculateFuelRequired(currentFuel);
+  }
+  return fuels;
+};
+
+export const puzzle1Part1 = () => {
   const modules = Deno
     .readTextFileSync("../data/mass_of_modules.txt")
     .split("\n");
 
-  const fuels = modules.map(fuel => calculateFuelRequired(fuel));
-  
+  const fuels = modules.map((fuel) => calculateFuelRequired(fuel));
+
   return sumOf(fuels, (fuel) => fuel);
 };
+
+export const puzzle1Part2 = (modules) => {
+  const fuels = modules.map((fuel) => calculateFuelForFuel(fuel));
+
+  const finalFuels = fuels.map((finalFuel) => sumOf(finalFuel, (fuel) => fuel));
+
+  return sumOf(finalFuels, (fuel) => fuel);
+};
+
+const main = () => {
+  const modules = Deno
+    .readTextFileSync("../data/mass_of_modules.txt")
+    .split("\n");
+  console.log(puzzle1Part2(modules));
+};
+
+main();
--- 2019/tests/Day1_test.js
+++ 2019/tests/Day1_test.js
@@ -1,5 +1,5 @@
 import { assertEquals } from "jsr:@std/assert";
-import { calculateFuelRequired } from "../src/Day1.js";
+import { calculateFuelForFuel, calculateFuelRequired, puzzle1Part2 } from "../src/Day1.js";
 
 Deno.test("calculate the fuel needed for the mass", () => {
   assertEquals(calculateFuelRequired(12), 2);
@@ -19,4 +19,34 @@ Deno.test("calculate the fuel needed for the mass", () => {
 
 Deno.test("calculate the fuel needed for the mass", () => {
   assertEquals(calculateFuelRequired(654), 216);
+});
+
+Deno.test("calculate the fuel needed for the fuel", () => {
+  assertEquals(calculateFuelForFuel(14), [2]);
+});
+
+Deno.test("calculate the fuel needed for the fuel", () => {
+  assertEquals(calculateFuelForFuel(70), [21, 5]);
+});
+
+Deno.test("calculate the fuel needed for the fuel", () => {
+  assertEquals(calculateFuelForFuel(1969), [654, 216, 70, 21, 5]);
+});
+
+Deno.test("calculate the fuel needed for the fuel", () => {
+  assertEquals(calculateFuelForFuel(100756), [
+    33583,
+    11192,
+    3728,
+    1240,
+    411,
+    135,
+    43,
+    12,
+    2,
+  ]);
+});
+
+Deno.test("puzzle1 part2", () => {
+  assertEquals(puzzle1Part2([100756]), 50346);
 });
