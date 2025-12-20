c4509e18efb0f79b1f21cee0e9d9d8994a141673
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2025-12-20 10:56:56 +0530

    Add day10 part1

---
+++ 2019/src/Day10.js
@@ -0,0 +1,53 @@
+import { maxBy } from "@std/collections";
+
+const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
+
+const detectCount = (originPoint, points) => {
+  const lineOfSightPoints = {};
+  let count = -1;
+
+  points.forEach((point) => {
+    const deltaX = originPoint.x - point.x;
+    const deltaY = originPoint.y - point.y;
+    const gcdOfDeltaValues = gcd(Math.abs(deltaX), Math.abs(deltaY)) || 1;
+    const pointToString = "x" + (deltaX / gcdOfDeltaValues) + "y" +
+      (deltaY / gcdOfDeltaValues);
+    let offset = 0;
+
+    if (!(pointToString in lineOfSightPoints)) {
+      lineOfSightPoints[pointToString] = point;
+      offset = 1;
+    }
+    count += offset;
+  });
+  return count;
+};
+
+const asteroidsDetections = (asteroids) =>
+  asteroids.map((asteroid) => ({
+    asteroid,
+    detectionCount: detectCount(asteroid, asteroids),
+  }));
+
+const parseString = (string) => string.split("\n").map((x) => x.split(""));
+
+const getPoints = (rows) => {
+  const points = [];
+  rows.forEach((row, i) => {
+    row.forEach((char, j) => {
+      char === "#" && points.push({ x: j, y: i });
+    });
+  });
+  return points;
+};
+
+const monitoringStation = (map) => {
+  const parsedMap = parseString(map);
+  const asteroidsLocations = getPoints(parsedMap);
+  const locations = asteroidsDetections(asteroidsLocations);
+  return maxBy(locations, ({ detectionCount }) => detectionCount);
+};
+
+const asteroidsMap = Deno.readTextFileSync("./data/day10_input.txt");
+
+console.log(monitoringStation(asteroidsMap));
