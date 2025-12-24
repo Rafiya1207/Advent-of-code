246319db3c83b44e0dc85bc4b27155f45980c258
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2025-12-24 23:41:53 +0530

    add day1 part2

--- 2015/day_1.js
+++ 2015/day_1.js
@@ -6,7 +6,17 @@ const floorOf = (instructions) =>
   instructions
     .reduce((floorNumber, instruction) => floorNumber + offset[instruction], 0);
 
+const firstBasementPosition = (instructions) => {
+  let currentFloorNumber = 0;
+
+  for (const index in instructions) {
+    const instruction = instructions[index];
+    if (currentFloorNumber === -1) return index;
+    currentFloorNumber += offset[instruction];
+  }
+};
+
 const input = Deno.readTextFileSync("./day_1_input.txt");
 const parsedInstructions = input.split("");
 
-console.log(floorOf(parsedInstructions));
+console.log(firstBasementPosition(parsedInstructions));
