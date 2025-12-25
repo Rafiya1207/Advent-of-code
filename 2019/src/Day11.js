f2d5f2c0b90f974efcd1fc2881ca363fedf7be52
Shaik Rafiya <shaikrafiya@Shaiks-MacBook-Pro.local>
2025-12-25 16:38:15 +0530

    add day11 part2

--- 2019/src/Day11.js
+++ 2019/src/Day11.js
@@ -15,20 +15,37 @@ const turnAndMoveRobot = (robot, nextMove) => {
 
   robot.position.x += robot.direction.dx * 1;
   robot.position.y += robot.direction.dy * 1;
+
+  robot.lowerBoundary.minX = Math.min(
+    robot.lowerBoundary.minX,
+    robot.position.x,
+  );
+  robot.lowerBoundary.minY = Math.min(
+    robot.lowerBoundary.minY,
+    robot.position.y,
+  );
+  robot.upperBoundary.maxX = Math.max(
+    robot.upperBoundary.maxX,
+    robot.position.x,
+  );
+  robot.upperBoundary.maxY = Math.max(
+    robot.upperBoundary.maxY,
+    robot.position.y,
+  );
 };
 
 const paint = ([color, direction], panels, robot) => {
   const key = "" + robot.position.x + "," + robot.position.y;
-  panels[key] = { color, position: robot.position };
+  panels[key] = { color, position: { ...robot.position } };
   turnAndMoveRobot(robot, direction);
 };
 
 const emergencyHull = (computer, panels, robot) => {
-  computer.inputs.push(0);
+  computer.inputs.push(1);
 
   while (!computer.isHalted) {
     stepForward(computer);
-
+    console.clear();
     if (computer.outputs.length === 2) {
       paint(computer.outputs, panels, robot);
       computer.outputs = [];
@@ -42,29 +59,38 @@ const emergencyHull = (computer, panels, robot) => {
 
 const input = Deno.readTextFileSync("./data/day11_input.txt");
 
-const part1 = () => {
+const part1 = (computer, panels, robot) => {
+  emergencyHull(computer, panels, robot);
+  console.log(Object.keys(panels).length);
+};
+
+const part2 = (panels, robot) => {
+  const image = [];
+
+  for (let y = robot.upperBoundary.maxY;y >= robot.lowerBoundary.minY; y--) {
+    let row = "";
+    for (let x = robot.lowerBoundary.minX; x < robot.upperBoundary.maxX; x++) {
+      const panel = "" + x + "," + y;
+      const color = !panels[panel] ? 0 : panels[panel].color;
+      const mark = color === 1 ? "#" : " ";
+      row += mark;
+    }
+    image.push(row);
+  }
+  return image.join('\n')
+};
+
+const main = () => {
   const program = parse(input);
   const computer = createComputer(program, 0);
   const panels = {};
   const robot = {
     position: { x: 0, y: 0 },
     direction: { dx: 0, dy: 1 },
+    lowerBoundary: { minX: Infinity, minY: Infinity },
+    upperBoundary: { maxX: 0, maxY: 0 },
   };
-
-  emergencyHull(computer, panels, robot);
-  console.log(Object.keys(panels).length);
-  return panels;
+  
+  part1(computer, panels, robot);
+  part2(panels, robot);
 };
-
-// const part2 = () => {
-//   const panels = part1();
-
-//   const image = [];
-//   Object.values(panels).forEach(({ color, position }) => {
-//     image[position.x][position.y] = color === 1 ? "#" : " "
-// });
-//   console.log(image);
-// };
-
-
-// part2();
